0fc572a572cc8cd0cf3c7ab4722277f7f2302dcd
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-03-20 15:28:51 -0400

    Add files via upload

--- MineSweeper.java
+++ MineSweeper.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 import java.io.*;
 import java.util.*;
 public class MineSweeper {
-    public int turns;
+    public boolean turns;
+    public static boolean gamestart;
     public static boolean win;
     public static boolean lose;
     public static String player;
@@ -12,26 +13,62 @@ public class MineSweeper {
     public static void main(String[]args) {
         Scanner scan;
         scan = new Scanner(System.in);
-        System.out.println("Welcome to MineSweeper, this isn't a sterotypical coding project I swear!.");
+        System.out.println("Welcome to MineSweeper, this isn't a sterotypical coding project I swear!");
         System.out.println("-------------------------------------------------------------------------");
         System.out.println("Now, lets get started, would you like to play, type Y for Yes and N for No.");
         String Answer = scan.nextLine();
         if(Answer.equals("Y")) {
             System.out.println("Alright Lets begin!");
-            BoardPrinter();
+            Rules();
         }
         if(Answer.equals("N")) {
             System.out.println("You are an Asshole for not Playing.");
         }
     }
+
     public static void BoardPrinter() {
         int[][]Board = new int [Rowamount][Columnamount];
         for(int Rowamount = 0; Rowamount < 12; ++Rowamount) {
             System.out.print("------------------------");
             System.out.println();
             for(int Columnamount = 0; Columnamount < 12; ++Columnamount) 
-                System.out.print(Board[Rowamount][Columnamount] + "|");
+                System.out.print(Board[Rowamount][Columnamount] + "|" );
                 System.out.println();
-            }
+            
         }
     }
+
+    public static void Rules() {
+        Scanner scan;
+        scan = new Scanner(System.in);
+        System.out.println("Before You start the game, if you have not played minesweeper, rules will be explained.");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("The Idea of the Game is simple, reveal all the Blocks in the Board, but do not uncover any mines.");
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println("Mines will look like this: #, and once one is revealed, and it is adjacent to the number block, the game ends.");
+        System.out.println("----------------------------------------------------------------------------------------------");
+        System.out.println("If you avoid all the mines, and Successfully Reveal all the Number Blocks, that look like this: |2|, |1|, |3|, etc.");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Once this Objective is completed, you win the game, giving if you have avoided all the mines successfully.");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Now, with all the rules explained, please type in Y or N if you wish to continue.");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+        String Answer = scan.next();
+        if(Answer.equals("Y")) {
+            System.out.println("The Game Shall Continue Now, Enjoy.");
+            gamestart();
+        }
+        if(Answer.equals("N")) {
+            System.out.println("Ok, the game shall now end.");
+        }
+    }
+
+    public static void gamestart() {
+        boolean gamestart = true;
+        while(gamestart = true) {
+            Scanner scan;
+            BoardPrinter();
+            break;
+        }
+    }
+}
